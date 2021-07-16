94e8baddcdb0ee0bcb99fbde6830ba524b7341e3
Konstantin Bondar <kostya.bondar@gmail.com>
2021-07-16 17:41:13 +0300

    Добавить иконку для WhatsApp

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -35,7 +35,8 @@ export default function Header(props) {
     <header className={classnames(props.className, 'header')}>
       <Link to='/' className={'header__link header__link_main'}><img src={main_logo} alt='лого Алрезка' className='header__main-logo' /></Link>
       <nav className='header__links-block header__contacts'>
-        <Link to='tel:+7(916)000-00-00' className={classnames('header__link')}>+7(916)000-00-00</Link>
+        <p className='header__text'>Санкт-Петербург</p>
+        <Link to='tel:+7(916)000-00-00' className='header__link'>+7(916)000-00-00</Link>
       </nav>
       <button className='header__burger' onClick={setNavigationOpen} />
       <Navigation isOpen={isNavigationOpen} handleClose={handleNavigationClose} handleClick={navigateToRoute} />
