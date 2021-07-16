5b9598ec160afe6c6196b6b74de0925bca589103
Konstantin Bondar <kostya.bondar@gmail.com>
2021-07-16 16:53:03 +0300

    Добавить титул в навигацию

--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -8,6 +8,7 @@ export default function Navigation(props) {
 
   return (
     <div className={classnames(props.className, 'nav-tab')}>
+      <HashLink to='/#' className='nav-tab__link nav-tab__link_title'>Алрезка</HashLink>
       <HashLink to='/#service' className='nav-tab__link'>Услуги</HashLink>
       <HashLink to='/#equipment' className='nav-tab__link'>Оборудование</HashLink>      
       <HashLink to='/#contacts' className='nav-tab__link'>Контакты</HashLink>
