fd1187b962c09e11e15ceeff2b756414ebc18bde
Konstantin Bondar <kostya.bondar@gmail.com>
2021-07-16 13:36:42 +0300

    промо

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -8,7 +8,7 @@ import classnames from 'classnames';
 export default function Promo(props) {
   return (
     <div className={classnames('promo', props.className)}>
-      <h1 className="promo__title">Начало сайта для алрезки.</h1>
+      <h1 className="promo__title">Компания «Алрезка» предлагает своим клиентам услугу алмазной резки проемов в стенах из бетона и железобетона. Это — современный способ резки, имеющий большие преимущества перед перфоратором и другими подобными инструментами. </h1>
       {/* <NavTab className="promo__nav-tab"/> */}
     </div>
   )
