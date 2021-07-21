f6bd777d96bfb60a5fbdadf58fafba5e05f71b72
Konstantin Bondar <kostya.bondar@gmail.com>
2021-07-21 15:57:43 +0300

    поправить формат

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -32,7 +32,7 @@ export default function Header(props) {
     <header className={classnames(props.className, 'header')}>
       <nav className='header__links-block header__contacts'>
         <p className='header__text'>Санкт-Петербург</p>
-        <a href={`tel:${CONTACTS.telRaw}`} className='header__link'>{CONTACTS.tel}</>
+        <a href={`tel:${CONTACTS.telRaw}`} className='header__link'>{CONTACTS.tel}</a>
       </nav>
       <button className='header__burger' onClick={setNavigationOpen} />
       <Navigation isOpen={isNavigationOpen} handleClose={handleNavigationClose} handleClick={navigateToRoute} />
