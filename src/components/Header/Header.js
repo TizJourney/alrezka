e879980c8fe7d38ca7953ec48c453cae8f9eb1c4
Konstantin Bondar <kostya.bondar@gmail.com>
2021-07-21 15:32:17 +0300

    Новая версия шапки и иконки

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,9 +5,6 @@ import './Header.css';
 import React, { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 
-import main_logo from '../../images/alrezka-logo.png';
-
-
 import Navigation from '../Navigation/Navigation.js';
 
 
@@ -33,7 +30,6 @@ export default function Header(props) {
 
   return (
     <header className={classnames(props.className, 'header')}>
-      <Link to='/' className={'header__link header__link_main'}><img src={main_logo} alt='лого Алрезка' className='header__main-logo' /></Link>
       <nav className='header__links-block header__contacts'>
         <p className='header__text'>Санкт-Петербург</p>
         <Link to='tel:+7(916)000-00-00' className='header__link'>+7(916)000-00-00</Link>
--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -3,12 +3,15 @@ import classnames from 'classnames';
 import './NavTab.css';
 import React from 'react';
 import { HashLink } from 'react-router-hash-link';
+import logoImage from '../../images/white-logo.png'
 
 export default function Navigation(props) {
 
   return (
     <div className={classnames(props.className, 'nav-tab')}>
-      <HashLink to='/#' className='nav-tab__link nav-tab__link_title'>Алрезка</HashLink>
+      <HashLink to='/#' className='nav-tab__link nav-tab__link_title'>
+        <img className='nav-tab__logo' src={logoImage} alt='Алрезка'/>
+      </HashLink>
       <HashLink to='/#service' className='nav-tab__link'>Услуги</HashLink>
       <HashLink to='/#equipment' className='nav-tab__link'>Оборудование</HashLink>      
       <HashLink to='/#contacts' className='nav-tab__link'>Контакты</HashLink>
