2d62626df82d3017cf341bd5f215aa5729913629
Konstantin Bondar <kostya.bondar@gmail.com>
2021-07-26 13:07:11 +0300

    первая версия разметки клиентов

--- src/components/Clients/Clients.js
+++ src/components/Clients/Clients.js
@@ -8,21 +8,32 @@ import { CLIENTS } from '../../utils/context';
 
 function ClientItem(props) {
     return (
-    <div className='client-item'>
-        <h2 className='client-item__title'>{props.title}</h2>
-        { props.descriptions &&
-            <ul className='client-item__description-block'>
-                {props.descriptions.map((item) => <li className='client-item__description-item'>{item}</li>)}
-            </ul>
-        }
-    </div>
+        <div className='client-item__item'>
+            <h3 className='client-item__title'>{props.title}</h3>
+            { props.descriptions &&
+                <ul className='client-item__description-block'>
+                    {props.descriptions.map((item) => <li className='client-item__description-item'>{item}</li>)}
+                </ul>
+            }
+        )
+        </div>
+    )
+}
+
+function ClientBlock(props) {
+    return (
+    <li className='client-item'>
+        { Object.entries(props.data).map(([title, descriptions], i) => <ClientItem key={i} title={title} descriptions={descriptions} />)}
+    </li>
     )
 }
 
 export default function Clients(props) {
     return (
         <div className={classnames('clients', props.className)}>
-            { Object.entries(CLIENTS).map(([key, value], i) => <ClientItem key={i} title={key} descriptions={value} />)}
+            <ul className='clients__grid-container'>
+                { CLIENTS.map((clientBlock, i) => <ClientBlock key={i} data={clientBlock} />)}
+            </ul>
         </div>
     )
 }
