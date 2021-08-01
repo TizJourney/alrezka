bfa075979ad264d9155edaaf9ad04dcc497d397e
Konstantin Bondar <kostya.bondar@gmail.com>
2021-08-01 15:08:27 +0300

    Добавить видео с примером работ

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -10,6 +10,7 @@ export default function Promo(props) {
   return (
     <div className={classnames('promo', props.className)}>
       <p className="promo__text">{PROMO}</p>
+      <iframe className="promo__video" title="Алрезка: видео" id="ytplayer" type="text/html" src="https://www.youtube.com/embed/DWyAGU1deB4" frameborder="0" allowfullscreen />
     </div>
   )
 }
