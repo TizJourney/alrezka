8e881d753a45fdd3f852dada0804973bf70aa9d6
Konstantin Bondar <kostya.bondar@gmail.com>
2021-08-10 12:07:19 +0300

    добавить бабель в проект

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+    entry: { main: './src/index.js' },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: '/node_modules/'
+            }
+        ]
+    }
+};
